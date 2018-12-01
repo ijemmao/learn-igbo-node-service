e7801f35a22a2d0b058bcc3bfbe722c0946f06de
ijemmao <ijemmao>
2018-11-30 18:39:21 -0800

    variables

--- src/test/test.js
+++ src/test/test.js
@@ -2,10 +2,11 @@ import axios from 'axios';
 
 const words = ['water', 'time', 'computer'];
 
-axios.get('http://localhost:8080/translate', {
+axios.get('http://learn-igbo.herokuapp.com/translate', {
   params: {
     words,
-  }
+  },
+  key: 'AIzaSyDCkvbIaUd2dguYB4LmpW0lq9_kUCfFC9k',
 })
   .then((res) => {
     console.log(res.data);
