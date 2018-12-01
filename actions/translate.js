d02ad2438bc3c73b333c15ed48ca423cb9271bca
ijemmao <ijemmao>
2018-11-30 17:55:36 -0800

    changed env vars

--- actions/translate.js
+++ actions/translate.js
@@ -1,7 +1,7 @@
 import { Translate } from '@google-cloud/translate';
-const env = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+const env = process.env.GOOGLE_TRANSLATE_PROJECT_ID;
 
-const projectId = env.project_id;
+const projectId = env;
 
 const translate = new Translate({
   projectId: projectId,
@@ -11,14 +11,3 @@ const text = 'water';
 const target = 'ig';
 
 export default translate;
-
-// translate
-//   .translate(text, target)
-//   .then(res => {
-//     const translation = res[0];
-//     console.log('Text: ', text);
-//     console.log('Translation: ', translation);
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   });
