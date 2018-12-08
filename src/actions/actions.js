3ed1ce0a22909a7050c51f23333fe7bf7c12aff2
ijemmao <ijemmao>
2018-12-07 20:17:54 -0800

    removed reference to firebase storage

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,7 +1,9 @@
+import admin from './firebase';
 import translate from './translate';
 import speech from './speech';
 
-import fs from 'fs';
+const database = admin.database();
+const bucket = admin.storage().bucket('learn-igbo');
 
 const translateInput = (req, res) => {
   console.log(req.headers);
@@ -26,41 +28,49 @@ const translateInput = (req, res) => {
 }
 
 const convertSpeech = (req, res) => {
-  console.log(req.query);
-  console.log(req.params);
-  console.log(req.data);
-  console.log(req.body);
-  // const audioBytes = req.params.audio;
-  const audioBytes = fs.readFileSync('src/test/my_audio_file.wav').toString('base64');
-  
-  const audio = {
-    content: audioBytes,
-  };
-  const config = {
-    encoding: 'LINEAR16',
-    sampleRateHertz: 44100,
-    languageCode: 'en-US',
-  };
-  const request = {
-    audio,
-    config,
-  };
 
-  speech.recognize(request).then(response => {
-    console.log('Response:', response[0].results);
-    const transcription = response[0].results
-      .map(result => result.alternatives[0].transcript)
-      .join('\n');
-    console.log('Transcription:', transcription);
-    return res.json(transcription);
-  })
-  .catch((error) => {
-    console.log('Error:', error);
-    return res.status(500).json({
-      error,
-      errorMessage: error.message
-    }); 
-  });
+
+  // storage.ref().child('audio').getDownloadURL().then((url) => {
+  //   axios.get(url).then((res) => {
+  //     console.log(res);
+  //   })
+  // })
+  // .catch((error) => {
+  //   res.status(500).json({ error });
+  // })
+
+
+  // database.ref(`/audio/${req.params.id}`).once('value')
+  // .then((snapshot) => {
+  //   const audioBytes = snapshot.val();
+  //   const audio = {
+  //     content: audioBytes,
+  //   };
+  //   const config = {
+  //     encoding: 'LINEAR16',
+  //     sampleRateHertz: 44100,
+  //     languageCode: 'en-US',
+  //   };
+  //   const request = {
+  //     audio,
+  //     config,
+  //   };
+  //   speech.recognize(request).then(response => {
+  //     const transcription = response[0].results
+  //       .map(result => result.alternatives[0].transcript)
+  //       .join('\n');
+  //     console.log('Transcription:', transcription);
+  //     return res.json(transcription);
+  //   })
+  //   .catch((error) => {
+  //     console.log('Error:', error);
+  //     return res.status(500).json({
+  //       error,
+  //       errorMessage: error.message
+  //     }); 
+  //   });
+  // })
+
 }
 
 export default { translateInput, convertSpeech };
