ae96f147852230a43c1c08a6e23cf095272f5797
ijemmao <ijemmao>
2018-12-08 16:16:33 -0800

    fixed babel

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -103,7 +103,8 @@ const downloadAudio = (req, res, database) => {
 const convertSpeech = (req, og_res) => {
   const client = new speech.SpeechClient();
 
-  const url = `https://learn-igbo.herokuapp.com/audio/${req.params.id}`;
+  // const url = `https://learn-igbo.herokuapp.com/audio/${req.params.id}`;
+  const url = `http://localhost:8080/audio/${req.params.id}`;
 
   axios({
     responseType: 'arraybuffer',
