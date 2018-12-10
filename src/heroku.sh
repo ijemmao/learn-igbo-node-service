a91d7e27c6fb572de1910df96efa96803d048ef4
ijemmao <ijemmao>
2018-12-09 18:56:12 -0800

    updated heroku script

--- src/heroku.sh
+++ src/heroku.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
-git add ./actions/firebase.js ./google.*
+git add .
+git commit -am "updating master"
+git push origin master --force
+git add ./actions/firebase.js ./google.* --force
 git commit -am "updating heroku"
 git push heroku master --force && git reset --hard HEAD~1
